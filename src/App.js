a76151fa23dd73f893def2c4a1ff9b26781d1dd4
MaximNovikov89 <mthelast@gmail.com>
2020-07-20 15:25:00 +0300

    starting the app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
