8d51cab2223b88a8cbb9a9b1aebd9df2cecb091f
MaximNovikov89 <mthelast@gmail.com>
2020-09-07 10:23:33 +0300

    Updates

---
+++ precache-manifest.bc6c6121bc8d2b3a059a6667e34ccbfa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1a7e30f12af2688d463fc7a4df1950",
+    "url": "/Post-Board/index.html"
+  },
+  {
+    "revision": "4b812fa160c615a8f8ec",
+    "url": "/Post-Board/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f9503f4bdd11af42437b",
+    "url": "/Post-Board/static/css/main.47a2345d.chunk.css"
+  },
+  {
+    "revision": "4b812fa160c615a8f8ec",
+    "url": "/Post-Board/static/js/2.9d3c9aec.chunk.js"
+  },
+  {
+    "revision": "ac00860f10b301ce4ff76aaa802ce348",
+    "url": "/Post-Board/static/js/2.9d3c9aec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9503f4bdd11af42437b",
+    "url": "/Post-Board/static/js/main.c5dd06d5.chunk.js"
+  },
+  {
+    "revision": "50740f61bf84f54842e9",
+    "url": "/Post-Board/static/js/runtime-main.b824885f.js"
+  },
+  {
+    "revision": "3c247b46f796829d85b8c0ab59c4ca02",
+    "url": "/Post-Board/static/media/IconLogo.3c247b46.png"
+  },
+  {
+    "revision": "3214051de23a247ac805d0007fe5735a",
+    "url": "/Post-Board/static/media/backGroundMain.3214051d.jpg"
+  }
+]);
